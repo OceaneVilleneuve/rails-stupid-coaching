87bc97ec10cb2721251d9e8549dcae1c5dcfc084
Oceane Villeneuve <oceanevilleneuve16@gmail.com>
2023-05-22 14:37:01 +0200

    stupid coach

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,19 @@
+class QuestionsController < ApplicationController
+
+  def ask
+
+  end
+
+  def answer
+    @answers = ['Great!', 'Silly question, get dressed and go to work!', "I don't care, get dressed and go to work!"]
+    @question = params[:question]
+
+    if @question == "I am going to work"
+      @answer = @answers[0]
+    elsif @question.include?('?')
+      @answer = @answers[1]
+    else
+      @answer = @answers[2]
+    end
+  end
+end
